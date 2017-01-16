e8d2c898abbcd99ab34e63ec863cb4e8feeda80d
Matt Bartholomew <Bartholomew.m134@gmail.com>
2017-01-16 15:59:43 -0500

    Created MainMenu Scene

---
+++ Assets/Scripts/PlayApplication.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayApplication : MonoBehaviour {
+
+	public void Play() {
+        // Play Game
+        Debug.Log("Play");
+        SceneManager.LoadScene("PONG");
+    }
+}
