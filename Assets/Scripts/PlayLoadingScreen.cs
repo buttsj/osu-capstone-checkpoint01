1143247ebab30870ddc66d46c95f63e5c1b0c7e1
John (Jack) Butts <butts.102@osu.edu>
2017-01-17 20:17:35 -0500

    added loading scene
    Doesn't switch scenes yet

---
+++ Assets/Scripts/PlayLoadingScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayLoadingScreen : MonoBehaviour {
+
+    public MovieTexture movie;
+    float timer = 0.0f;
+
+    void Start()
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        movie.Play();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 12.0f)
+        {
+            Debug.Log("movie is done");
+            // load next scene
+            timer = 0.0f;
+        }
+     }
+
+}
