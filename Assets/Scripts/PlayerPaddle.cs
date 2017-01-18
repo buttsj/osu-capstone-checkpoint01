933593ebdcc490851bb17dd317368f2b4c8a752b
John <cramer240@gmail.com>
2017-01-18 10:35:40 -0500

    Started creating PONG

---
+++ Assets/Scripts/PlayerPaddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaddle : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-16.0f * Time.deltaTime , 0.0f, 0.0f));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(16.0f * Time.deltaTime, 0.0f, 0.0f));
+        }
+	}
+}
