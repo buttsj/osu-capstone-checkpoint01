a71f6d4b7cbbd5729e9c517cbbcda37015987f3d
John <cramer240@gmail.com>
2017-01-18 14:06:52 -0500

    Made changes to PingPong

--- Assets/Scripts/BallBehavior.cs
+++ Assets/Scripts/BallBehavior.cs
@@ -8,11 +8,15 @@ public class BallBehavior : MonoBehaviour {
 
     private GameObject paddle1;
 
+    private Rigidbody rb;
+
 	void Start () {
 
         active = false;
 
         paddle1 = GameObject.Find("Paddle1");
+
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	void Update () {
@@ -20,13 +24,10 @@ public class BallBehavior : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             active = true;
+            rb.AddForce(new Vector3(0.0f, 0.0f, 30.0f), ForceMode.VelocityChange);
         }
 
-        if (active)
-        {
-
-        }
-        else
+        if (!active)
         {
             transform.position = new Vector3(paddle1.transform.position.x, paddle1.transform.position.y, paddle1.transform.position.z + 1);
         }
