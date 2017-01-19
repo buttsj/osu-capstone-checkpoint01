b40a16d24932d44be0174f1270c51caaef915508
connor-swick <swick.64@osu.edu>
2017-01-19 13:51:10 -0500

    Added video capture
    may lag

---
+++ Assets/VRCapture/Demo/Scripts/VideoCaptureManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace VRCapture.Demo {
+
+    public class VideoCaptureManager : MonoBehaviour {
+
+        bool enableMainCamera = false;
+        bool enable360Camera = false;
+        bool enableTopDownCamera = true;
+        bool enableLeftRightCamera = true;
+        bool enableOnlyAudio = false;
+
+        void Start() {
+            VRCapture.Instance.RegisterSessionCompleteDelegate(HandleCaptureFinish);
+            //Application.runInBackground = true;
+        }
+
+        void OnGUI() {
+            enableMainCamera = GUI.Toggle(
+                new Rect(50, 50, 150, 50),
+                enableMainCamera,
+                " Enable Video Camera");
+            if (enableMainCamera) {
+                VRCapture.Instance.GetCaptureVideo(0).isEnabled = true;
+            }
+            else {
+                VRCapture.Instance.GetCaptureVideo(0).isEnabled = false;
+            }
+            if (GUI.Button(new Rect(50, 350, 150, 50), "Capture Start")) {
+                print("Capture Start");
+                VRCapture.Instance.BeginCaptureSession();
+            }
+            if (GUI.Button(new Rect(50, 450, 150, 50), "Capture Stop")) {
+                print("Capture Stop");
+                VRCapture.Instance.EndCaptureSession();
+            }
+            if (GUI.Button(new Rect(50, 550, 150, 50), "Open Video Folder")) {
+                System.Diagnostics.Process.Start(VRCapture.Instance.FolderPath);
+            }
+        }
+
+        void HandleCaptureFinish() {
+            print("Capture Finish");
+        }
+    }
+}
---
+++ Assets/VRCapture/Scripts/VRConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace VRCapture {
+    /// <summary>
+    /// Basic config for VRCapture.
+    /// </summary>
+    public class VRCaptureConfig {
+        public const string CAPTURE_FOLDER = "VRCapture";
+        public const string REPLAY_FOLDER = "VRCapture/VRReplay";
+        public const string FFMPEG_WIN_PATH = "VRCapture/FFmpeg/Win/ffmpeg.exe";
+        public const string FFMPEG_MAC_PATH = "VRCapture/FFmpeg/Mac/ffmpeg";
+    }
+    /// <summary>
+    /// Basic config for VRReplay.
+    /// </summary>
+    public class VRReplayConfig {
+        public const int HTC_VIVE_DEVICE_NUM = 16;
+    }
+}
