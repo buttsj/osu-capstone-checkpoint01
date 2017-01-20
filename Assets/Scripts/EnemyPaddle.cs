dd89e920c546a32edce4cb7993cf38c655d8cfa4
powerskyle26 <powers.411@osu.edu>
2017-01-20 12:06:33 -0500

    Updated enemy AI, changed colors

--- Assets/Scripts/EnemyPaddle.cs
+++ Assets/Scripts/EnemyPaddle.cs
@@ -6,15 +6,17 @@ public class EnemyPaddle : MonoBehaviour {
     Rigidbody rb;
     GameObject ball;
     Vector3 paddleVelocity;
+    Vector3 wanderVelocity;
 
 	void Start () {
         ball = GameObject.Find("Ball");
         rb = GetComponent<Rigidbody>();
         paddleVelocity = new Vector3(30f, 0, 0);
+        wanderVelocity = new Vector3(20f, 0, 0);
 	}
 	
 	void FixedUpdate () {
-        if (ball.GetComponent<Rigidbody>().velocity.z > 0)
+        if (ball.GetComponent<Rigidbody>().velocity.z > 0 && ball.transform.position.z > -8)
         {
             float positionDifference = transform.position.x - ball.transform.position.x;
             if (positionDifference < -1)
@@ -31,7 +33,13 @@ public class EnemyPaddle : MonoBehaviour {
             }
         }
         else {
-            rb.velocity = Vector3.zero;
+            rb.velocity = wanderVelocity;
         }
 	}
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.name.Contains("Wall")) {
+            wanderVelocity = -wanderVelocity;
+        }
+    }
 }
