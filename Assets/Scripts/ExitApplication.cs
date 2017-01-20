e31d157c065c5e828fd6cf6f42658c7802370675
Matt Bartholomew <Bartholomew.m134@gmail.com>
2017-01-20 17:54:50 -0500

    Updated Menu with Sound/Images

--- Assets/Scripts/ExitApplication.cs
+++ Assets/Scripts/ExitApplication.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class ExitApplication : MonoBehaviour {
 
     public void Exit() {
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay() {
+        // Delay for Button
+        yield return new WaitForSeconds(0.65f);
+
+        // Quit Application
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
--- Assets/Scripts/PlayApplication.cs
+++ Assets/Scripts/PlayApplication.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class PlayApplication : MonoBehaviour {
 
-	public void Play() {
+	public void Play() {  
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay() {
+        // Delay Button Press
+        yield return new WaitForSeconds(0.65f);
+
         // Play Game
         SceneManager.LoadScene("PingPong");
     }
